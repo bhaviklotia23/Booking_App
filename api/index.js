a315491119e0bc784e99849d6a7e0db4835dd2ab
blotia-stacksum <bhavikl.stacksum@gmail.com>
2022-11-07 18:02:10 +0530

    Update : Create and Update Hotel API completed

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
 
 const app = express();
 dotenv.config();
@@ -8,23 +13,25 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("Connected to MongoDB")
+    console.log("Connected to MongoDB");
   } catch (error) {
     throw error;
   }
 };
 
-mongoose.connection.on("disconnected", ()=>{
-    console.log("mongoDB disconnected")
-})
-
-mongoose.connection.on("connected", () => {
-  console.log("mongoDB connected");
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
 });
 
-app.get("/", (req, res)=>{
-    res.send("Hello first request")
-})
+// middleware
+
+app.use(express.json())
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
 
 app.listen(8000, () => {
   connect();
