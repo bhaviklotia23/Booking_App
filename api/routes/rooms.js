247ada43c8dc1de5867e2f6e27a91b513aba5eba
blotia-stacksum <bhavikl.stacksum@gmail.com>
2022-11-09 15:39:02 +0530

    Update : Rooms API completed

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,24 @@
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoomById,
+  getRooms,
+  updateRoom
+} from "../controller/roomController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-
-
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+//UPDATE
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+router.get("/:id", getRoomById);
+//GET ALL
+router.get("/", getRooms);
 
 export default router;
